69e3d5957a83ae8cbb284a06bace9ec560df1249
bladstrom <jonathanbladstrom@icloud.com>
2017-09-26 06:17:17 -0700

    add test in main

---
+++ CalculatorApp/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr1;
+            //int nr2;
+            Console.WriteLine("Ange första siffran: ");
+            nr1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("värdet av nr1 är: " + nr1);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
