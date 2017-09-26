f53538983ed1b16bfa69fad147a0ad64660bd35a
bladstrom <jonathanbladstrom@icloud.com>
2017-09-26 06:52:56 -0700

    added constants for Calculator

--- CalculatorApp/ConsoleApp1/Program.cs
+++ CalculatorApp/ConsoleApp1/Program.cs
@@ -11,12 +11,24 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int nr1;
-            //int nr2;
-            Console.WriteLine("Ange första siffran: ");
-            nr1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("värdet av nr1 är: " + nr1);
-            Console.ReadLine();
+            const string divide = "/";
+            const string add = "+";
+            const string sub = "-";
+            const string mult = "*";
+            
 
+            //int nr2;
+            Console.Write("Ange första siffran: ");
+            nr1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("värdet av nr1 är " + nr1 + ". Vad vill du använda: '/' '+' '-' '*'");
+            // ReadLine metod för att writeline ska köras
+            
+           
+ 
+
+
+
+            Console.ReadLine();
 
             
         }
